a7aa11c10595f2c67b45d4b2e19992402fe612e3
yjcyxky <yjcyxky@163.com>
2022-11-11 09:08:19 +0800

    [Fix Bug] index out of bounds when run a single module.

--- src/qc/mod.rs
+++ src/qc/mod.rs
@@ -15,16 +15,16 @@ use std::sync::Arc;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct QCResults {
     filemeta: Option<hasher::Meta>,
-    fastqc: fastqc::FastQC,
-    vaf_matrix: mislabeling::VAFMatrix,
+    fastqc: Option<fastqc::FastQC>,
+    vaf_matrix: Option<mislabeling::VAFMatrix>,
 }
 
 impl QCResults {
-    pub fn fastqc(&self) -> &fastqc::FastQC {
+    pub fn fastqc(&self) -> &Option<fastqc::FastQC> {
         return &self.fastqc;
     }
 
-    pub fn vaf_matrix(&self) -> &mislabeling::VAFMatrix {
+    pub fn vaf_matrix(&self) -> &Option<mislabeling::VAFMatrix> {
         return &self.vaf_matrix;
     }
 
@@ -43,17 +43,18 @@ impl QCResults {
         which: Arc<String>,
     ) -> QCResults {
         match parse_path(Some(fastq_path), |parser| {
+            let which_arc = Arc::clone(&which);
             let result: Result<Vec<_>, Error> =
                 parser.parallel_each(n_threads, move |record_sets| {
+                    let which_step = &which_arc[..];
                     let mut qc = fastqc::FastQC::new(&contaminants, &adapters);
                     let mut vaf_matrix = mislabeling::VAFMatrix::new(count, &count_vec);
                     for record_set in record_sets {
                         for record in record_set.iter() {
-                            let which_step = &which[..];
                             if which_step == "fastqc" || which_step == "all" {
                                 qc.process_sequence(&record);
                             }
-                            
+
                             if which_step == "checkmate" || which_step == "all" {
                                 vaf_matrix.process_sequence_unsafe(&patterns, &record);
                             }
@@ -62,31 +63,49 @@ impl QCResults {
 
                     QCResults {
                         filemeta: None,
-                        fastqc: qc,
-                        vaf_matrix: vaf_matrix,
+                        fastqc: Some(qc),
+                        vaf_matrix: Some(vaf_matrix),
                     }
                 });
 
             return match result {
                 Ok(qc_results) => {
-                    let mut merged_qc = qc_results[0].fastqc().to_owned();
-                    let mut merged_vaf_matrix = qc_results[0].vaf_matrix().to_owned();
+                    let which_step = &which[..];
+                    let mut merged_qc = qc_results[0].fastqc().to_owned().unwrap();
+                    let mut merged_vaf_matrix = qc_results[0].vaf_matrix().to_owned().unwrap();
                     for i in 1..qc_results.len() {
-                        merged_qc.merge(&[qc_results[i].fastqc().to_owned()]);
-                        merged_vaf_matrix.merge(&[qc_results[i].vaf_matrix().to_owned()]);
+                        if which_step == "fastqc" || which_step == "all" {
+                            if let Some(fastqc) = qc_results[i].fastqc().to_owned() {
+                                merged_qc.merge(&[fastqc]);
+                            }
+                        }
+
+                        if which_step == "checkmate" || which_step == "all" {
+                            if let Some(vaf_matrix) = qc_results[i].vaf_matrix().to_owned() {
+                                merged_vaf_matrix.merge(&[vaf_matrix]);
+                            }
+                        }
                     }
 
-                    merged_qc.finish();
+                    let fastqc = if which_step == "fastqc" || which_step == "all" {
+                        merged_qc.finish();
+                        let filename = Path::new(fastq_path).file_name().unwrap().to_str().unwrap();
+                        Some(merged_qc.update_name(filename))
+                    } else {
+                        None
+                    };
 
-                    let filename = Path::new(fastq_path).file_name().unwrap().to_str().unwrap();
-                    let fastqc = merged_qc.update_name(filename);
-
-                    merged_vaf_matrix.finish();
+                    let vaf_matrix = if which_step == "checkmate" || which_step == "all" {
+                        merged_vaf_matrix.finish();
+                        Some(merged_vaf_matrix)
+                    } else {
+                        None
+                    };
 
                     QCResults {
                         filemeta: None,
                         fastqc: fastqc,
-                        vaf_matrix: merged_vaf_matrix,
+                        vaf_matrix: vaf_matrix,
                     }
                 }
                 Err(msg) => {
@@ -117,22 +136,34 @@ impl QCResults {
                 .each(|record| {
                     if which == "fastqc" || which == "all" {
                         qc.process_sequence(&record);
-                    } else if which == "checkmate" || which == "all" {
+                    }
+
+                    if which == "checkmate" || which == "all" {
                         vaf_matrix.process_sequence_unsafe(&patterns, &record);
                     }
-                    
+
                     return true;
                 })
                 .expect("Invalid fastq file");
 
-            let filename = Path::new(fastq_path).file_name().unwrap().to_str().unwrap();
+            let fastqc = if which == "fastqc" || which == "all" {
+                let filename = Path::new(fastq_path).file_name().unwrap().to_str().unwrap();
+                qc.finish();
+                Some(qc.update_name(filename))
+            } else {
+                None
+            };
 
-            qc.finish();
-            vaf_matrix.finish();
+            let vaf_matrix = if which == "checkmate" || which == "all" {
+                vaf_matrix.finish();
+                Some(vaf_matrix)
+            } else {
+                None
+            };
 
             QCResults {
                 filemeta: None,
-                fastqc: qc.update_name(filename),
+                fastqc: fastqc,
                 vaf_matrix: vaf_matrix,
             }
         }) {
